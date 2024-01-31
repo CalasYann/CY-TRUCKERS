7f00ef425248ce3c3804306d2652ee922b37c583
CalasYann <133207210+CalasYann@users.noreply.github.com>
2024-01-31 20:54:03 +0100

    Add files via upload

---
+++ progc/Tri.h
@@ -0,0 +1,12 @@
+#ifndef TRI_H
+
+	#define TRI_H
+	#include <stdlib.h>
+	#include <stdio.h>
+	#include "AVL.h"
+	
+	void TriSelection(Route* T, int n);
+	void max50(AVL* a, Route* R);
+	void parcoursInfixe( AVL* a, Route* R);
+	
+#endif
