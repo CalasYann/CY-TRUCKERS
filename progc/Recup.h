8abcd4a15ad5163339cfae96dfa87cf95854aead
CalasYann <133207210+CalasYann@users.noreply.github.com>
2024-02-02 18:42:43 +0100

    Update Recup.h

--- progc/Recup.h
+++ progc/Recup.h
@@ -5,5 +5,7 @@
 	
 	void recupID(char* c, int t);
 	void recupDist(char* c, int t);
-	
+	void recupVille(char * c, int t);
+	void recupEtape(char* c, int t);
+	void recupDriver(char * c, int t);
 #endif
