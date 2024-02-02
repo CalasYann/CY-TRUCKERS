45642c3aaf5e0d1f2cd5e79dc3f9709418f03096
CalasYann <133207210+CalasYann@users.noreply.github.com>
2024-02-02 18:43:40 +0100

    Update Tri.c

--- progc/Tri.c
+++ progc/Tri.c
@@ -1,7 +1,7 @@
 #include "Tri.h"
 
 
-void TriSelection(Route* T, int n){ //Tri le tableau dans l'ordre décroissant.
+void TriSelection(Route* T, int n){
 	int noEtape,noCase,indiceMin;
 	Route temp;
 	
@@ -20,7 +20,7 @@ void TriSelection(Route* T, int n){ //Tri le tableau dans l'ordre décroissant.
 	}
 }
 
-void max50(AVL* a, Route* R){ //rentre dans les 50 routes dont la différence entre le plus grand trajet et le plus petit est la plus grande.
+void max50(AVL* a, Route* R){
 	a->r.diff=a->r.max-a->r.min;
 	a->r.moyenne=(a->r.somme/a->r.trajet)/1000;
 	if(R[0].diff<=a->r.diff){
@@ -33,7 +33,7 @@ void max50(AVL* a, Route* R){ //rentre dans les 50 routes dont la différence en
 
 
 
-void parcoursInfixe( AVL* a, Route* R){//Parcours l'arbre en vérifiant si chaque élément fait parti du top 50
+void parcoursInfixe( AVL* a, Route* R){
 	if(a!=NULL){
 		parcoursInfixe(a->fg,R);
 		max50(a,R);
@@ -41,3 +41,45 @@ void parcoursInfixe( AVL* a, Route* R){//Parcours l'arbre en vérifiant si chaqu
 		free(a);
 	}
 }
+
+
+
+void triVSelection(Ville * t, int n){
+    int i, j, index_min;
+    Ville temp;
+
+    for (i = 0; i<n-1; i++){
+        index_min = i;
+        for (j = i+1; j<n; j++){
+            if(t[j].nombreTrajets < t[index_min].nombreTrajets){
+                index_min = j;
+            }
+        }if(index_min != i){
+             temp = t[index_min];
+             t[index_min] = t[i];
+             t[i] = temp;
+              
+        }
+    }
+
+}
+
+void max10(Ville * v, AVLvilles  * a){
+	if (v[0].nombreTrajets <= a->nombreTrajets){
+		v[0].nombreTrajets = a->nombreTrajets;
+		strcpy(v[0].nom, a->nom);
+		v[0].nbPremierEtape=a->nbPremierEtape;
+		//rajouter trajets max;
+		triVSelection(v, 10);
+	}
+}
+
+void parcoursVinfixe(AVLvilles * a, Ville *  v){
+	if (a != NULL){
+		parcoursVinfixe(a->fg, v);
+		max10(v, a);
+		parcoursVinfixe(a->fd, v);
+		free(a);
+	}
+}
+
