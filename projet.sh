124df1af4a7092f1aef3c69e4ecc8815da56d4d2
CalasYann <133207210+CalasYann@users.noreply.github.com>
2024-02-02 18:42:14 +0100

    Update projet.sh

--- projet.sh
+++ projet.sh
@@ -5,7 +5,7 @@ all_arg=$*
 arg1=$1
 
 for var in $all_arg ; do
-	if [ $var = -h ] ; then #Affiche le manuel de la fonction
+	if [ $var = -h ] ; then
 		echo "Un de vos argument vaut -h, voici le manuel d'aide de la commande : 
 Cette commande est associé a plusieur option, pour l'utilisé correctement il faut mettre en premier argument votre fichier à traiter comme indiqué dans le ReadMe associé.
 Les différentes options :
@@ -17,16 +17,16 @@ Les différentes options :
 	exit 
 	fi
 done
-term=`echo $arg1 | sed 's/.*\.//g' ` #récupère l'extension du fichier passé en paramètre.
+term=`echo $arg1 | sed 's/.*\.//g' `
 
 
-if [ $nb_arg -lt 2 ] ; then #Vérifie le nombre d'argument passé en paramètre
+if [ $nb_arg -lt 2 ] ; then
 	echo "Nombre d'argument trop faible"
 	exit
-elif [ ! -f $arg1 ] ; then #Vérifie que le premier paramètre est un fichier
+elif [ ! -f $arg1 ] ; then
 	echo "Votre premier argument n'est pas un fichier"
 	exit
-elif [ "$term" != csv ] ; then #Vérifie que l'extension du fichier est bien un csv
+elif [ "$term" != csv ] ; then
 
 	echo "Votre fichier n'est pas un fichier .csv"
 	exit
@@ -40,7 +40,7 @@ exec=0
 
 
 for var in $dosc/* ; do #prend le contenu du dossier
-	if [ -x $var ] ; then #Vérifie si l'executable existe déjà
+	if [ -x $var ] ; then 
 	echo "L'executable existe déjà"
 	exec=$var;
 	fi
@@ -48,7 +48,7 @@ done
 
 if [ $exec == "0" ] ; then 
 	for var in $dosc/* ; do #prend le contenu du dossier
-		if [ `echo $var | sed 's/.*\.//g' ` == c ] ; then #Compile le fichier c
+		if [ `echo $var | sed 's/.*\.//g' ` == c ] ; then 
 		
 		cd progc/
 		exec=`make`
@@ -63,17 +63,17 @@ fi
 
 ttemp=0 
 for var in $doss/* ; do 
-	if [ "$var" == "$doss/temp" ] ; then #Vide le dossier temp si il existe.
+	if [ `echo $var | sed 's/.*\///g' ` == temp ] ; then 
 		`rm -r -f temp/*`
 		ttemp=1
 	fi
 done
 
-if [ $ttemp == "0" ] ; then #Crée le dossier temp s'il n'existe pas
+if [ $ttemp == "0" ] ; then 
 	`mkdir temp`
 fi
 
-if [ ! -e images/ ] ; then #Crée le dossier images s'il n'existe pas
+if [ ! -e images/ ] ; then 
 	`mkdir images`
 fi
 
@@ -81,9 +81,9 @@ count=1
 
 for var in $all_arg ; do
 
-	if [ $var == "-d1" ] ; then #fait le traitement d1 si il y a le bonne argument.
+	if [ $var == "-d1" ] ; then 
 		echo "Traitement -d1"
-		time awk -F';' 'NR > 1 { !DRIVE[$1";"$6]++ }END {for(a in DRIVE) print a ";" DRIVE[a]}' "$arg1" | awk -F';' '{ game[$2] += 1}END {for (a in game) printf ("%s;%s\n", a, game[a])}' | sort -n -r -t";" -k2 | head -10 > temp/tempd1.dat #récupère les valeurs utiles et les place dans un fichier. 
+		time awk -F';' 'NR > 1 { !DRIVE[$1";"$6]++ }END {for(a in DRIVE) print a ";" DRIVE[a]}' "$arg1" | awk -F';' '{ game[$2] += 1}END {for (a in game) printf ("%s;%s\n", a, game[a])}' | sort -n -r -t";" -k2 | head -10 > temp/tempd1.dat
 		gnuplot <<EOF
 		set datafile separator ";"
 		set terminal pngcairo enhanced font 'Verdana,8' 
@@ -111,14 +111,12 @@ for var in $all_arg ; do
 
 		set terminal wxt
 EOF
-#crée le graphique
-		convert -rotate 90 images/temphistogrammed1.png images/histogrammed1.png #le tourne dans le bon sens
+		convert -rotate 90 images/temphistogrammed1.png images/histogrammed1.png
 		rm images/temphistogrammed1.png
-
 		
-	elif [ $var == "-d2" ] ; then #fait le traitement d2 si il y a le bonne argument.
+	elif [ $var == "-d2" ] ; then
 		echo "Traitement -d2"
-		time awk -F';' 'NR > 1 { NAME[$6]+=$5 }END { for(M in NAME)printf("%s;%.3f\n",M,NAME[M]) }' "$arg1" | sort -t";" -k2 -r -n | head -10 > temp/tempd2.dat #récupère les valeurs utiles et les place dans un fichier.
+		time awk -F';' 'NR > 1 { NAME[$6]+=$5 }END { for(M in NAME)printf("%s;%.3f\n",M,NAME[M]) }' "$arg1" | sort -t";" -k2 -r -n | head -10 > temp/tempd2.dat
 		gnuplot <<EOF
 		set datafile separator ";"
 		set terminal pngcairo enhanced font 'Verdana,8' 
@@ -146,14 +144,13 @@ EOF
 
 		set terminal wxt
 EOF
-#crée le graphique
 		convert -rotate 90 images/temphistogrammed2.png images/histogrammed2.png
 		rm images/temphistogrammed2.png
 
-	elif [ $var == "-l" ] ; then #fait le traitement l si il y a le bonne argument.
+	elif [ $var == "-l" ] ; then
 		echo "Traitement -l en cours"
 		
-		time awk -F';' 'NR > 1 { NAME[$1]+=$5 }END { for(M in NAME)printf("%s;%.3f\n",M,NAME[M]) }' "$arg1" | sort -t";" -k2 -r -n | head -10 > temp/templ.dat #récupère les valeurs utiles et les place dans un fichier.
+		time awk -F';' 'NR > 1 { NAME[$1]+=$5 }END { for(M in NAME)printf("%s;%.3f\n",M,NAME[M]) }' "$arg1" | sort -t";" -k2 -r -n | head -10 > temp/templ.dat
 
 		gnuplot <<EOF
 		set datafile separator ";"
@@ -170,32 +167,31 @@ EOF
 		set xlabel 'Route ID'
 		plot 'temp/templ.dat' using 2:xtic(1) title'Distance' lc rgb "red"
 EOF
-#crée le graphique
 
-	elif [ $var == "-t" ] ; then #fait le traitement t si il y a le bonne argument. 
+
+	elif [ $var == "-t" ] ; then 
 		echo "Traitement -t"	
-  		gnuplot <<EOF
+		time  tac "$arg1" | cut -d";" -f1,2,3,4,6 | ./progc/exec 2
+		gnuplot <<EOF
 		set datafile separator ";"
 		set terminal pngcairo enhanced font 'Verdana,8' 
-		set output 'histogrammet.png'
-		
+		set output 'images/histogrammet.png'
+
 		set style data histograms 
 		set style histogram clustered
 		set style fill solid
 		set boxwidth 1.5
-		
+
 		set ytics nomirror
 		set ylabel 'Distance' 
 		set xlabel 'Route ID'
-		plot 'tempt.dat' using 2:xtic(1) title'Total' lc rgb "red",\
-		'tempt.dat' using 3:xtic(1) title'Départ' lc rgb "blue" 
+		plot 'temp/tempt.dat' using 2:xtic(1) title'Total' lc rgb "red",\
+		'temp/tempt.dat' using 3:xtic(1) title'Départ' lc rgb "blue" 
 EOF
-	
 
-		#faire le traitement T
-	elif [ $var == "-s" ] ; then #fait le traitement s si il y a le bonne argument.
+	elif [ $var == "-s" ] ; then
 		echo "Traitement -s" 
-		time  tac "$arg1" | cut -d";" -f1,5 | ./progc/exec 1 #appelle le programme c pour réaliser le traitement S et mettre dans un fichier les résultats.
+		time  tac "$arg1" | cut -d";" -f1,5 | ./progc/exec 1
 
 
 		gnuplot <<EOF
@@ -223,7 +219,6 @@ EOF
 	
 
 EOF
-#crée le graphique
 	elif [ $count == "1" ] ; then
 		echo " "
 	else 
@@ -232,6 +227,7 @@ EOF
 	count=$((count+1))		
 	
 done
+
 
 cd progc
 a=`make clean`
